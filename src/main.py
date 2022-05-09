5c630777167dc6ac500f4ef2cbf98f42e6820cef
BAaboe <benjamin.mjaatvedt@gmail.com>
2022-05-09 10:14:34 +0200

    added coments and made the game window dependt on cols and rows

--- src/main.py
+++ src/main.py
@@ -3,15 +3,31 @@ import random
 
 pygame.init()
 
-WIDTH, HEIGHT = 700, 800
 
 BG_COLOR = "white"
 
-ROWS = 15
-COLS = 15
-MINES = 20
+ROWS = input("Rows: ")
+COLS = input("Cols: ")
+MINES = input("Mines: ")
+if ROWS == "":
+    ROWS = 15
+else:
+    ROWS = int(ROWS)
+if COLS == "":
+    COLS = 15
+else:
+    COLS = int(COLS)
+if MINES == "":
+    MINES = 20
+else:
+    MINES = int(MINES)
 
-SIZE = WIDTH / ROWS
+print("ROWS: ", ROWS)
+print("COLS: ", COLS)
+print("MINES: ", MINES)
+
+SIZE = 50
+WIDTH, HEIGHT = SIZE*COLS, SIZE*ROWS+100    
 
 NUM_FONT = pygame.font.SysFont("comicsans-regular", 20)
 NUM_COLORS = {1: "black", 2: "green", 3: "blue", 4: "orange", 5: "red", 6: "purple", 7: "pink", 8: "yellow"}
@@ -24,6 +40,9 @@ pygame.display.set_caption("GoinSweeper")
 pygame.display.set_icon(GOIN)
 
 def get_neighbors(row, col, rows, cols):
+    """
+    Retruns a list of all the neighbor squares to a given squares
+    """
     neighbors = []
 
     if row > 0:
@@ -47,6 +66,9 @@ def get_neighbors(row, col, rows, cols):
     return neighbors
 
 def make_map(rows, cols, mines):
+    """
+    Makes a map of the game containing where the bombs are placed
+    """
     field = [[0 for i in range(cols)] for i in range(rows)]
     mine_positions = set()
 
@@ -70,31 +92,46 @@ def make_map(rows, cols, mines):
     
     return field
 
+def make_covered_map(rows, cols):
+    """
+    Makes a list containing what squares is covered
+    """
+    cover_field = [[1 for i in range(cols)] for i in range(rows)]
+    return cover_field
 
-def draw(field):
+def draw(field, cover_field):
+    """
+    Draws to the screen
+    """
     WINDOW.fill(BG_COLOR)
 
     for i ,row in enumerate(field):
         y = SIZE*i
         for j, value in enumerate(row):
             x = SIZE*j
-            pygame.draw.rect(WINDOW, (200,200,200), (x,y, SIZE, SIZE))
-            pygame.draw.rect(WINDOW, "black", (x,y, SIZE, SIZE), 2)
-            if value > 0:
-                text = NUM_FONT.render(str(value), 1, NUM_COLORS[value])
-                WINDOW.blit(text, (x+(SIZE/2 - text.get_width()/2), y+(SIZE/2 - text.get_height()/2)))
-            elif value == -1:
-                WINDOW.blit(GOIN, (x,y, SIZE, SIZE))
-
-
+            if cover_field[i][j] == 0:
+                pygame.draw.rect(WINDOW, (200,200,200), (x,y, SIZE, SIZE))
+                pygame.draw.rect(WINDOW, "black", (x,y, SIZE, SIZE), 2)
+                if value > 0:
+                    text = NUM_FONT.render(str(value), 1, NUM_COLORS[value])
+                    WINDOW.blit(text, (x+(SIZE/2 - text.get_width()/2), y+(SIZE/2 - text.get_height()/2)))
+                elif value == -1:
+                    WINDOW.blit(GOIN, (x,y, SIZE, SIZE))
+            else:
+                pygame.draw.rect(WINDOW, (100,100,100), (x,y, SIZE, SIZE))
+                pygame.draw.rect(WINDOW, "black", (x,y, SIZE, SIZE), 2)
 
     pygame.display.update()
 
 def main():
+    """
+    Game loop
+    """
     running = True
 
 
     field = make_map(ROWS, COLS, MINES)
+    cover_field = make_covered_map(ROWS, COLS)
 
     while running:
         for event in pygame.event.get():
@@ -105,7 +142,7 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     running = False
                     break
-        draw(field)
+        draw(field, cover_field)
     
     pygame.quit()
 
